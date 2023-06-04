c1e50f00804c3a88921abc9c18d89e7817ec208d
Diego Faria <diego.rdfaria@gmail.com>
2023-06-04 01:20:09 -0400

    create a simple fake database

---
+++ SignalRChat/Database/FakeDatabase.cs
@@ -0,0 +1,19 @@
+﻿using SignalRChat.Models;
+
+namespace SignalRChat.Database
+{
+    public class FakeDatabase : IFakeDatabase
+    {
+        private readonly List<Message> _fakeDatabase = new();
+
+        public IList<Message> GetAllMessages()
+        {
+            return _fakeDatabase.OrderBy(m => m.CreatedAt).ToList();
+        }
+
+        public void AddMessage(Message message)
+        {
+            _fakeDatabase.Add(message);
+        }
+    }
+}
---
+++ SignalRChat/Database/IFakeDatabase.cs
@@ -0,0 +1,10 @@
+﻿using SignalRChat.Models;
+
+namespace SignalRChat.Database
+{
+    public interface IFakeDatabase
+    {
+        void AddMessage(Message message);
+        IList<Message> GetAllMessages();
+    }
+}
