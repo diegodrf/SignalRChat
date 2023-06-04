1a288b5e37425e66d73aaae27e21b61f48eb7710
Diego Faria <diego.rdfaria@gmail.com>
2023-06-04 01:20:39 -0400

    create models

---
+++ SignalRChat/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalRChat.Models
+{
+    public class Message
+    {
+        public User User { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; }
+
+        public Message(User user, string text)
+        {
+            User = user;
+            Text = text;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
