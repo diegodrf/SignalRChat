1dc05e3c12a1c12db3c22a2bb8a10e85d7a2574f
Diego Faria <diego.rdfaria@gmail.com>
2023-06-04 01:24:14 -0400

    refactor to load old messages when start

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -8,16 +8,29 @@ var connection = new signalR
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+function showMessage(text) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    li.textContent = text;
+}
+
+connection.on("ReceiveMessage", function (user, message) {
+    showMessage(`${user} says ${message}`);
+});
+
+connection.on("AllMessages", function (messages) {
+    messages.forEach(m => {
+        showMessage(`${m.user.name} says ${m.text}`);
+    });
 });
 
 connection.start().then(function () {
+    connection.invoke('GetHistory').catch(function (err) {
+        return console.error(err.toString());
+    });
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
